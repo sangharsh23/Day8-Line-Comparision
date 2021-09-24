9e5e0dd671fbd0ff44dbfb58dfb63676c688427c
sangharsh23 <sangharsh355@gmail.com>
2021-09-24 18:29:44 +0530

    Added : welcome msg

---
+++ src/com/bridgelabz/line/comparision/LineComparision.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.line.comparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the line comparision program");
+
+	}
+
+}
